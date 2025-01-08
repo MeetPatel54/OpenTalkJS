8afee1b2ead7118b200976d5e0e99ea67953d797
Pmu782005 <pmeet782005@gmail.com>
2025-01-08 10:51:25 +0530

    new Added

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
+// import ollama from "ollama";
+// const response = await ollama.chat({
+//     model: "llama3.1:8b",
+//     messages: [{ role: "user", content: "Generate marketing emails" }],
+// });
+// console.log(response.message.content);
+
+// ================= fetch Question From One File And Save the Response to another file ========
+
+import fs from 'fs';
 import ollama from "ollama";
+const q = fs.readFileSync("q.txt", "utf8");
 const response = await ollama.chat({
-    model: "llama3.1:8b",
-    messages: [{ role: "user", content: "Generate marketing emails" }],
+model: "llama3.1:8b",
+messages: [{ role: "user", content: q}],
 });
-console.log(response.message.content);
+const a =response.message.content;
+fs.writeFileSync("a.txt", a);
